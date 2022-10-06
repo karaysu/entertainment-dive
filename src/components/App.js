cc252c7d72178dc7814e145bd991983a4a0b2815
karaysu <sukhpreets6718@gmail.com>
2022-10-06 00:32:32 -0400

    add - animation for logo and search bar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Routes from './Routes'
 
+import './main.scss';
+
 function App() {
 	return (
 		<React.Fragment>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,35 +9,49 @@ import ScrollToTop from '../../components/ScrollToTop/ScrollToTop'
 
 import NoPoster from '../../assests/images/no_poster.jpg'
 
+import { useState } from 'react'
+
 function Home(props) {
 
-  //Passing the onSearchSubmit to Infinite Scrolling
-  InfiniteScrolling(props.onSearchSubmit);
-  
-  //Rendering the API cards
-	const renderCards = arr => (
-		arr.map((card, index) => {
-			return <Card key={index} id={card.id} title={card.title} poster={card.poster_path ? card.poster_path : NoPoster} doesHavePoster = {card.poster_path != null} />
-    })
-	)
-	
-	return (
-		<div className="home">
-			<h1 className="website-name">Entertainment Dive</h1>
-			<SearchBar
-				className="home-searchbar"
-        searchText={props.searchText}
-				setSearchText={props.setSearchText}
-        onSearchSubmit={props.onSearchSubmit}
-        emptySearchResults={props.emptySearchResults}
-			/>
-			<div className="card-container">
-        {renderCards(props.searchResults)}
-      </div>
-			<ScrollToTop />
-      {/* <button className="test" onClick={props.emptySearchResults} type="submit">Test</button> */}
-		</div>
-	);
+    const [isSearchAcitve, setIsSearchActive] = useState(false);
+
+    //Passing the onSearchSubmit to Infinite Scrolling
+    InfiniteScrolling(props.onSearchSubmit);
+
+    //Rendering the API cards
+    const renderCards = arr => (
+        arr.map((card, index) => {
+            return <Card key={index} id={card.id} title={card.title} poster={card.poster_path ? card.poster_path : NoPoster} doesHavePoster={card.poster_path != null} />
+        })
+    )
+
+    const handleSearchSubmit = () => {
+        // Update the UI
+        setIsSearchActive(true);
+
+        props.onSearchSubmit();
+    }
+
+    return (
+        <div className="home">
+            <div className={`header-container ${isSearchAcitve ? "header-active" : ""}`}>
+                <h1 className="website-name">Entertainment Dive</h1>
+                <div className='home-searchbar'>
+                    <SearchBar
+                        searchText={props.searchText}
+                        setSearchText={props.setSearchText}
+                        onSearchSubmit={handleSearchSubmit}
+                        emptySearchResults={props.emptySearchResults}
+                    />
+                </div>
+            </div>
+            <div className="card-container">
+                {renderCards(props.searchResults)}
+            </div>
+            <ScrollToTop />
+            {/* <button className="test" onClick={props.emptySearchResults} type="submit">Test</button> */}
+        </div>
+    );
 }
 
 export default Home;
